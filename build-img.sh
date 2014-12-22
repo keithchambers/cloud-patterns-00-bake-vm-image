9e8838ebd2703488e580acceb1397f0f541326fa
Keith Chambers <chambers_keith@yahoo.com>
2014-12-22 07:45:21 +0000

    Redirect 'command -v' output to /dev/null.

--- build-img.sh
+++ build-img.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-command -v ansible-playbook
+command -v ansible-playbook >/dev/null 2>&1
 if [ $? -ne 0 ]; then
     sudo yum install -y epel-release
     sudo yum install -y ansible

b126182138d5208817b38f9e6b0bc64724729c9c
Keith Chambers <chambers_keith@yahoo.com>
2015-01-04 18:59:14 -0800

    Formatting cleanup

--- build-img.sh
+++ build-img.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+set -eo pipefail
  
+# test is ansible installed
 command -v ansible-playbook >/dev/null 2>&1
-if [ $? -ne 0 ]; then
+if [[ "$?" -ne 0 ]]; then
     sudo yum install -y epel-release
     sudo yum install -y ansible
 fi
  
 ansible-playbook ./lib/build-img.yml
-RC=$?
+RETVAL="$?"
 
-echo "Completed in $SECONDS seconds."
+echo "Completed in $SECONDS seconds"
  
-exit "$RC"
+exit "$RETVAL"
  
 # EOF

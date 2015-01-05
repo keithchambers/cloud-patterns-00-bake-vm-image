7fb530c611c43c4b7b6e8609f7f3d193c0372ecc
Keith Chambers <chambers_keith@yahoo.com>
2015-01-05 00:00:33 -0800

    Update build-img.sh

--- build-img.sh
+++ build-img.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
- 
-# test is ansible installed
-command -v ansible-playbook >/dev/null 2>&1
-if [[ "$?" -ne 0 ]]; then
+
+# test is ansible rpm installed
+if [[ -z "$(rpm -qa ansible)" ]]; then
     sudo yum install -y epel-release
     sudo yum install -y ansible
 fi
- 
-ansible-playbook ./lib/build-img.yml
+
+# build image
+export ANSIBLE_HOST_KEY_CHECKING=False
+ansible-playbook lib/build-img.yml
 RETVAL="$?"
 
-echo "Completed in $SECONDS seconds"
- 
+# exit with details
+[[ "$RETVAL" -eq 0 ]] && echo -n "OK" || echo -n "ERROR"
+echo " in $SECONDS seconds"
 exit "$RETVAL"
- 
+
 # EOF

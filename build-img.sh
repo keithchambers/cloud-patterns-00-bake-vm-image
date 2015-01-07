ab9f456b905f3dd6064014f3e775c30d0191c421
Keith Chambers <chambers_keith@yahoo.com>
2015-01-06 16:52:23 -0800

    Update build-img.sh

--- build-img.sh
+++ build-img.sh
@@ -11,10 +11,10 @@ export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook lib/build-img.yml
 RETVAL="$?"
 
-# exit with details
-echo "Build image "
-[[ "$RETVAL" -eq 0 ]] && echo -n "SUCCEEDED" || echo -n "FAILED"
-echo " in $SECONDS seconds"
+# print build result
+[[ "$RETVAL" -eq 0 ]] && RETMSG="SUCCEEDED" || RETMSG="FAILED"
+echo "Build image $RETMSG in $SECONDS seconds."
+
 exit "$RETVAL"
 
 # EOF
